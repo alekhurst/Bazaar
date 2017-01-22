ad3615ae6b455703dba0840d6b571619111578fa
Alek Lord Hurst <alek@Aleks-MacBook-Pro.local>
2017-01-21 23:44:04 -0800

    Refactor environment.js

--- app/hammer/environment.js
+++ app/hammer/environment.js
@@ -1,11 +1,39 @@
-//export const serverUrl = "https://bazaar-api-stg.herokuapp.com";
-export const serverUrl = "https://bazaar-api-prod.herokuapp.com";
+const STAGING = 'STAGING';
+const PRODUCTION = 'PRODUCTION';
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyBC9CfCfZ4Br-P-XK27QTLv2hfQGMTOVqc",
-  authDomain: "fir-bazaar.firebaseapp.com",
-  databaseURL: "https://fir-bazaar.firebaseio.com",
-  storageBucket: "firebase-bazaar.appspot.com",
-};
+/* ---- MODIFY THIS LINE ----------- */
+const CURRENT_ENVIRONMENT = STAGING;
+/* ----------------------------------*/
 
-export const appVersion = '1.0.29';
+let staging = {
+  serverUrl: "https://bazaar-api-stg.herokuapp.com",
+}
+
+let production = {
+  serverUrl: "https://bazaar-api-prod.herokuapp.com"
+}
+
+let both = {
+  appVersion: '1.0.29',
+  firebaseConfig: {
+    apiKey: "AIzaSyBC9CfCfZ4Br-P-XK27QTLv2hfQGMTOVqc",
+    authDomain: "fir-bazaar.firebaseapp.com",
+    databaseURL: "https://fir-bazaar.firebaseio.com",
+    storageBucket: "firebase-bazaar.appspot.com",
+  },
+}
+
+let envConfig = {};
+switch (CURRENT_ENVIRONMENT) {
+  case STAGING:
+    envConfig = Object.assign({}, staging, both);
+    break;
+  case PRODUCTION:
+    envConfig = Object.assing({}, production, both);
+    break;
+  default:
+    envConfig = Object.assign({}, staging, both);
+    break;
+}
+
+module.exports = envConfig;
